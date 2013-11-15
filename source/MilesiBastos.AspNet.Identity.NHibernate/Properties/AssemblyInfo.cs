2f52e8459c9e9a71ccfa375f25e9160b81f3b7fd
Antonio Milesi Bastos <antonio@milesibastos.com>
2013-11-15 17:27:48 -0200

    remove modules
    add nuget packages

--- source/MilesiBastos.AspNet.Identity.NHibernate/Properties/AssemblyInfo.cs
+++ source/MilesiBastos.AspNet.Identity.NHibernate/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MilesiBastos.AspNet.Identity.NHibernate")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © milesibastos.com 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.2000")]
+[assembly: AssemblyInformationalVersion("4.0.0-Beta-0")]
+[assembly: AssemblyVersion("4.0.0.0")]

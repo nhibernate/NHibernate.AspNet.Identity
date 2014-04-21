4c67ce79530d89d6a8127b51e16657098fb71946
Martin Thwaites <github@my2cents.co.uk>
2014-04-21 22:24:04 +0100

    Added MappingHelper to map the identity models for NHibernate

---
+++ source/NHibernate.AspNet.Identity/Helpers/MappingHelper.cs
@@ -0,0 +1,55 @@
+﻿using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernate.AspNet.Identity.Helpers
+{
+    public static class MappingHelper
+    {
+
+        /// <summary>
+        /// Gets a mapping that can be used with NHibernate.
+        /// </summary>
+        /// <param name="additionalTypes">Additional Types that are to be added to the mapping, this is useful for adding your ApplicationUser class</param>
+        /// <returns></returns>
+        public static HbmMapping GetIdentityMappings(System.Type[] additionalTypes)
+        {
+            var baseEntityToIgnore = new[] { 
+                typeof(NHibernate.AspNet.Identity.DomainModel.EntityWithTypedId<int>), 
+                typeof(NHibernate.AspNet.Identity.DomainModel.EntityWithTypedId<string>), 
+            };
+
+            var allEntities = new List<System.Type> { 
+                typeof(IdentityUser), 
+                typeof(IdentityRole), 
+                typeof(IdentityUserLogin), 
+                typeof(IdentityUserClaim),
+            };
+            allEntities.AddRange(additionalTypes);
+
+            var mapper = new ConventionModelMapper();
+            DefineBaseClass(mapper, baseEntityToIgnore.ToArray());
+            mapper.IsComponent((type, declared) => typeof(NHibernate.AspNet.Identity.DomainModel.ValueObject).IsAssignableFrom(type));
+
+            mapper.AddMapping<IdentityUserMap>();
+            mapper.AddMapping<IdentityRoleMap>();
+            mapper.AddMapping<IdentityUserClaimMap>();
+
+            return mapper.CompileMappingFor(allEntities);
+        }
+
+        private static void DefineBaseClass(ConventionModelMapper mapper, System.Type[] baseEntityToIgnore)
+        {
+            if (baseEntityToIgnore == null) return;
+            mapper.IsEntity((type, declared) =>
+                baseEntityToIgnore.Any(x => x.IsAssignableFrom(type)) &&
+                !baseEntityToIgnore.Any(x => x == type) &&
+                !type.IsInterface);
+            mapper.IsRootEntity((type, declared) => baseEntityToIgnore.Any(x => x == type.BaseType));
+        }
+    }
+}

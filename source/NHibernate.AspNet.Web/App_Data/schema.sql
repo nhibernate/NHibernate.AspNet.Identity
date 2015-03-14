b0c728e5f1e301d1dd62b928ba037bf094235500
antonio@milesibastos.com <Antonio Milesi Bastos>
2015-03-14 10:15:16 -0700

    update to mysql

--- source/NHibernate.AspNet.Web/App_Data/schema.sql
+++ source/NHibernate.AspNet.Web/App_Data/schema.sql
@@ -1,5 +1,23 @@
 
-    PRAGMA foreign_keys = OFF
+    
+alter table AspNetUserRoles  drop foreign key FK86803B282B87AB2A
+
+
+    
+alter table AspNetUserRoles  drop foreign key FK86803B28EA778823
+
+
+    
+alter table AspNetUserLogins  drop foreign key FKEF896DAEEA778823
+
+
+    
+alter table AspNetUserClaims  drop foreign key FKF4F7D992EA778823
+
+
+    
+alter table ApplicationUser  drop foreign key FK4376B148E75DF37
+
 
     drop table if exists AspNetUsers
 
@@ -13,54 +31,78 @@
 
     drop table if exists ApplicationUser
 
-    PRAGMA foreign_keys = ON
-
     create table AspNetUsers (
-        Id TEXT not null,
-       AccessFailedCount INT,
-       Email TEXT,
-       EmailConfirmed BOOL,
-       LockoutEnabled BOOL,
+        Id VARCHAR(255) not null,
+       AccessFailedCount INTEGER,
+       Email VARCHAR(255),
+       EmailConfirmed TINYINT(1),
+       LockoutEnabled TINYINT(1),
        LockoutEndDateUtc DATETIME,
-       PasswordHash TEXT,
-       PhoneNumber TEXT,
-       PhoneNumberConfirmed BOOL,
-       TwoFactorEnabled BOOL,
-       UserName TEXT not null unique,
-       SecurityStamp TEXT,
+       PasswordHash VARCHAR(255),
+       PhoneNumber VARCHAR(255),
+       PhoneNumberConfirmed TINYINT(1),
+       TwoFactorEnabled TINYINT(1),
+       UserName VARCHAR(255) not null unique,
+       SecurityStamp VARCHAR(255),
        primary key (Id)
     )
 
     create table AspNetUserRoles (
-        UserId TEXT not null,
-       RoleId TEXT not null,
-       constraint FKFAADC1EF92E2FD93 foreign key (RoleId) references AspNetRoles,
-       constraint FKFAADC1EF526E4265 foreign key (UserId) references AspNetUsers
+        UserId VARCHAR(255) not null,
+       RoleId VARCHAR(255) not null
     )
 
     create table AspNetUserLogins (
-        UserId TEXT not null,
-       LoginProvider TEXT,
-       ProviderKey TEXT,
-       constraint FK6B768E3C526E4265 foreign key (UserId) references AspNetUsers
+        UserId VARCHAR(255) not null,
+       LoginProvider VARCHAR(255),
+       ProviderKey VARCHAR(255)
     )
 
     create table AspNetRoles (
-        Id TEXT not null,
-       Name TEXT not null unique,
+        Id VARCHAR(255) not null,
+       Name VARCHAR(255) not null unique,
        primary key (Id)
     )
 
     create table AspNetUserClaims (
-        Id  integer primary key autoincrement,
-       ClaimType TEXT,
-       ClaimValue TEXT,
-       UserId TEXT,
-       constraint FKE3450235526E4265 foreign key (UserId) references AspNetUsers
+        Id INTEGER NOT NULL AUTO_INCREMENT,
+       ClaimType VARCHAR(255),
+       ClaimValue VARCHAR(255),
+       UserId VARCHAR(255),
+       primary key (Id)
     )
 
     create table ApplicationUser (
-        applicationuser_key TEXT not null,
-       primary key (applicationuser_key),
-       constraint FKBF196D2F8745E746 foreign key (applicationuser_key) references AspNetUsers
+        applicationuser_key VARCHAR(255) not null,
+       primary key (applicationuser_key)
     )
+
+    alter table AspNetUserRoles 
+        add index (RoleId), 
+        add constraint FK86803B282B87AB2A 
+        foreign key (RoleId) 
+        references AspNetRoles (Id)
+
+    alter table AspNetUserRoles 
+        add index (UserId), 
+        add constraint FK86803B28EA778823 
+        foreign key (UserId) 
+        references AspNetUsers (Id)
+
+    alter table AspNetUserLogins 
+        add index (UserId), 
+        add constraint FKEF896DAEEA778823 
+        foreign key (UserId) 
+        references AspNetUsers (Id)
+
+    alter table AspNetUserClaims 
+        add index (UserId), 
+        add constraint FKF4F7D992EA778823 
+        foreign key (UserId) 
+        references AspNetUsers (Id)
+
+    alter table ApplicationUser 
+        add index (applicationuser_key), 
+        add constraint FK4376B148E75DF37 
+        foreign key (applicationuser_key) 
+        references AspNetUsers (Id)

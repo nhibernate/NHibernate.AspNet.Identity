98007463c378f0d41917c1c3e6f33d74abbb4207
antonio@milesibastos.com <Antonio Milesi Bastos>
2015-03-22 08:11:36 -0700

    Remove the Assembly*Version attributes from your Properties\AssemblyInfo.cs

--- source/NHibernate.AspNet.Identity.Tests/Properties/AssemblyInfo.cs
+++ source/NHibernate.AspNet.Identity.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("18e8d596-f8e2-470a-b599-4c9b19d81252")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- source/NHibernate.AspNet.Identity/Properties/AssemblyInfo.cs
+++ source/NHibernate.AspNet.Identity/Properties/AssemblyInfo.cs
@@ -21,17 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8e361ffa-d318-49fb-be07-33e7066a1849")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("2.2")]
-[assembly: AssemblyInformationalVersion("2.2")]
-[assembly: AssemblyVersion("2.2")]

327fe5db609b9ecd2c6ee5cefe30554f0b258d1c
Antonio Milesi Bastos <antonio@milesibastos.com>
2015-05-05 16:57:18 -0300

    eliminating duplicate code

--- source/NHibernate.SessionResolver/SessionFactoryBuilder.cs
+++ source/NHibernate.SessionResolver/SessionFactoryBuilder.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using NHibernate.Cfg;
-using NHibernate.Context;
-using NHibernate.Engine;
 using NHibernate.Owin;
 using Owin;
 
@@ -24,50 +21,12 @@ namespace NHibernate
 
         public void Build(IAppBuilder app)
         {
-            var resolver = new DefaultSessionResolver();
             foreach (var configuration in _configurations) {
                 var factory = configuration.BuildSessionFactory();
-                resolver.RegisterFactoryToResolve(factory);
+                SessionResolver.RegisterFactoryToResolve(factory);
             }
 
-            app.Use<SessionMiddleware>(resolver);
-            SessionResolver.SetResolver(resolver);
-        }
-
-        private class DefaultSessionResolver : ISessionResolver
-        {
-            private static readonly ISet<ISessionFactory> _factories;
-
-            static DefaultSessionResolver()
-            {
-                _factories = new HashSet<ISessionFactory>();
-            }
-
-            internal void RegisterFactoryToResolve(ISessionFactory factory)
-            {
-                _factories.Add(factory);
-            }
-
-            public IEnumerable<ISessionFactory> GetAllFactories()
-            {
-                return _factories;
-            }
-
-            public ISessionFactory GetFactoryFor<TEntity>()
-            {
-                var type = typeof(TEntity);
-                var factory = _factories.SingleOrDefault(x => ((ISessionFactoryImplementor)x).TryGetEntityPersister(type.FullName) != null);
-                return factory;
-            }
-
-            public ISession GetCurrentSessionFor<TEntity>()
-            {
-                var factory = GetFactoryFor<TEntity>();
-                if (!CurrentSessionContext.HasBind(factory))
-                    CurrentSessionContext.Bind(factory.OpenSession());
-
-                return factory.GetCurrentSession();
-            }
+            app.Use<SessionMiddleware>(SessionResolver.Current);
         }
     }
 }
